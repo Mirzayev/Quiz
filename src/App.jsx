aabdebd78133a0fd0cd875f9deb2369913f0583a
Lazizbek-Narzullayev <lazizbeknarzullayev1810@gmail.com>
2024-12-07 16:34:28 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css'
 import Admin from "./pages/admin-page/pages/Admin.jsx";
+import Login from './pages/admin-page/pages/Login.jsx';
+import Register from './pages/admin-page/pages/Registr.jsx';
 
 function App() {
 
     return (
         <div className=''>
+            <Login/>
+            <Register/>
             <Admin/>
         </div>
     )
---
+++ src/pages/admin-page/pages/Registr.jsx
@@ -0,0 +1,51 @@
+
+import LogoWhite from '../../../assets/images/Logo-white.png'
+import LoginImg from '../../../assets/images/LoginImg.png'
+import React from "react"
+function Register(){
+    return (
+        <div className="w-full min-h-screen bg-[#0D1321] flex justify-center items-center">
+            <div className="w-[1440px] h-[720px] bg-[#0D1321] p-[20px] flex justify-around">
+                <div className='max-w-[50%] flex flex-col py-[40px]'>
+                    <img src={LogoWhite} className='w-[150px] py-[20px]' alt="" />
+                    <h1 className='text-[#C5D86D] text-[25px] py-[15px]'>Create your account and start using QuizWiz!</h1>
+                    <div className='flex flex-col gap-[20px]'>  
+                        <div className='flex justify-around gap-[20px]'>
+                            <button className='w-[190px] h-[130px] gap-[10px] flex flex-col items-center p-[20px] bg-[#333333] rounded-[10px]  text-[white] text-[15px] font-bold'><i class="fa-solid fa-user-tie text-white text-[45px]">‌</i>Sign in as a tutor</button>
+                            <button className='w-[190px] h-[130px] gap-[10px] flex flex-col items-center p-[20px] bg-[#333333] rounded-[10px]  text-[white] text-[15px] font-bold'><i class="fa-solid fa-user-graduate text-white text-[45px]">‌</i>Sign in as a learner</button>
+                            <button className='w-[190px] h-[130px] gap-[10px] flex flex-col items-center p-[20px] bg-[#333333] rounded-[10px]  text-[white] text-[15px] font-bold border-[5px] border-[#C5D86D]'><i class="fa-solid fa-user-plus text-[#C5D86D] text-[45px]">‌</i>Create Account</button>
+                        </div>
+                        <form className='flex flex-col gap-[20px]'>
+                            <div className='flex justify-between gap-[15px]'>
+                                <div className='flex flex-col w-[50%]'>
+                                    <label className='text-white text-[16px] p-[7px] font-bold'>Your first name</label>
+                                    <input className='w-full h-[40px] p-[15px] text-[white] rounded-[10px] placeholder:text-[white] bg-[#0D1321] border-[3px] border-[white]' type="email" placeholder='Type your first name'/>
+                                </div>
+
+                                <div className='flex flex-col w-[50%]'>
+                                    <label className='text-white text-[16px] p-[7px] font-bold'>Your last name</label>
+                                    <input className='w-full h-[40px] p-[15px] text-[white] rounded-[10px] placeholder:text-[white] bg-[#0D1321] border-[3px] border-[white]' type="email" placeholder='Type your last name'/>
+                                </div>
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='text-white text-[16px] p-[7px] font-bold'>Your email address</label>
+                                <input className='w-full h-[40px] p-[15px] text-[white] rounded-[10px] placeholder:text-[white] bg-[#0D1321] border-[3px] border-[white]' type="email" placeholder='Type your email'/>
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='text-white text-[16px] p-[7px] font-bold'>Password</label>
+                                <input className='w-full h-[40px] p-[15px] text-[white] rounded-[10px] placeholder:text-[white] bg-[#0D1321] border-[3px] border-[white]' type="password" placeholder='Password'/>
+                            </div>
+                        </form>
+                        <div>
+                            <button className='bg-[#F5F5F5] text-[#000000] py-[10px] px-[20px] rounded-[10px]'>Sign Un <i class="fa fa-check-circle text-[black]" aria-hidden="true"></i></button>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <img src={LoginImg} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Register
